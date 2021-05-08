d8eff44c60d50fd696159008b7c2978a147daef6
Light1603 <59634280+Light1603@users.noreply.github.com>
2021-05-08 22:11:04 +0700

    Update login_form.js

--- new Login form & Profile/login_form.js
+++ new Login form & Profile/login_form.js
@@ -1,27 +1,19 @@
 
-const email = document.forms['form']['email']
+/*
+var email = document.forms['form']['email']
 var password = document.forms['form']['password']
+
+*/
+
+var email = document.querySelector('#input_email');
+var password = document.querySelector('#input_password');
 
 var email_error = document.getElementById('error_email')
 var password_error = document.getElementById('error_password')
 
 
-email.addEventListener('textInput', email_validate);
-password.addEventListener('textInput', password_validate);
-
 function validation() {
-    var mail_format = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-    if (!(email.value.match(mail_format))) {
-        email.style.border = "1px solid red";
-        email_error.style.display="block";
-        email.focus();
-        alert("Invalid email/password provided.");
-        return false;
-    }
     if (password.value !== "password") {
-        password.style.border = "1px solid red";
-        password_error.style.display="block";
-        password.focus();
         alert("Invalid email/password provided.");
         return false;
     }
@@ -36,25 +28,31 @@ function email_validate() {
 }
 
 function password_validate(){
-    if (passwordl.value.length >=7) {
-        passwordl.style.border = "1px solid silver";
+    if (password.value.length >=7) {
+        password.style.border = "1px solid silver";
         password_error.style.display = "none";
         return true;
     }
 }
 
-/* update profile email */
+email.addEventListener('keyup', function() {
+    var mail_format = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    if (!(email.value.match(mail_format))) {
+        email.style.border = "1px solid red";
+        email_error.style.display="block";
+        email.focus();
+    }
+})
 
-function getParameterByName(name, url) {
-    if (!url) url = window.location.href;
-    name = name.replace(/[\[\]]/g, '\\$&');
-    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
-        results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+email.addEventListener('textInput', email_validate);
+password.addEventListener('textInput', password_validate);
+
+//Email update
+
+function getEmail() {
+    let email = document.getElementById('input_email').value;
+    localStorage.setItem('Email',email);
 }
 
-var emailUpdated = getParameterByName('input_email');
 
-document.getElementById("emails").innerHTML = emailUpdated;
+
